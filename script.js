6a0fba6a4128baf6b9578a2d1a0bff306d56f0a8
Wiatroffka <99502879+Wiatroffka@users.noreply.github.com>
2023-09-04 15:24:25 +0200

    Adding photos, some styling in css

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Initialize GSAP
-gsap.registerPlugin(ScrollTrigger);
 
-// Create the parallax effect
-gsap.to('.parallax-bg', {
-    y: -200, // Adjust this value to control the parallax effect
-    ease: 'none',
-    scrollTrigger: {
-        trigger: '.parallax-section',
-        start: 'top top',
-        end: 'bottom bottom',
-        scrub: true,
-    },
+const rooms = document.querySelector('.word-list');
+const carousel = document.querySelector('.carousel');
+
+
+const images = {
+    Przedpokój: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+    Sypialnia: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+    Łazienka: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+    Kuchnia: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+    Salon: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+    Balkon: ['apple1.jpg', 'apple2.jpg', 'apple3.jpg'],
+};
+
+rooms.addEventListener('click', (e) => {
+    const word = e.target.textContent;
+
+    if (images[word]) {
+        displayImages(images[word]);
+    }
 });
+
+function displayImages(imageArray) {
+    carousel.innerHTML = '';
+
+    imageArray.forEach((imageSrc) => {
+        const img = document.createElement('img');
+        img.src = imageSrc;
+        carousel.appendChild(img);
+    });
+
+    $('.carousel').slick({
+        slidesToShow: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+}
+
